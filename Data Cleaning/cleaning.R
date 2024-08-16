738aa64132aaf10c745f57145efaf6860534b542
ItachiPrabin <itachiprabin@gmail.com>
2024-08-16 23:45:08 +0545

    Cleaned data

---
+++ Data Cleaning/cleaning.R
@@ -0,0 +1,246 @@
+library(tidyverse)
+
+#Crime
+#2022 Dataset of Bristol
+bristol_1_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-01/2022-01-avon-and-somerset-street.csv")
+bristol_2_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-02/2022-02-avon-and-somerset-street.csv")
+bristol_3_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-03/2022-03-avon-and-somerset-street.csv")
+bristol_4_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-04/2022-04-avon-and-somerset-street.csv")
+bristol_5_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-05/2022-05-avon-and-somerset-street.csv")
+bristol_6_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-06/2022-06-avon-and-somerset-street.csv")
+bristol_7_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-07/2022-07-avon-and-somerset-street.csv")
+bristol_8_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-08/2022-08-avon-and-somerset-street.csv")
+bristol_9_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-09/2022-09-avon-and-somerset-street.csv")
+bristol_10_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-10/2022-10-avon-and-somerset-street.csv")
+bristol_11_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-11/2022-11-avon-and-somerset-street.csv")
+bristol_12_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-12/2022-12-avon-and-somerset-street.csv")
+
+#2023 Dataset of Bristol
+bristol_1_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-01/2023-01-avon-and-somerset-street.csv")
+bristol_2_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-02/2023-02-avon-and-somerset-street.csv")
+bristol_3_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-03/2023-03-avon-and-somerset-street.csv")
+bristol_4_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-04/2023-04-avon-and-somerset-street.csv")
+bristol_5_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-05/2023-05-avon-and-somerset-street.csv")
+bristol_6_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-06/2023-06-avon-and-somerset-street.csv")
+bristol_7_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-07/2023-07-avon-and-somerset-street.csv")
+bristol_8_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-08/2023-08-avon-and-somerset-street.csv")
+bristol_9_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-09/2023-09-avon-and-somerset-street.csv")
+bristol_10_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-10/2023-10-avon-and-somerset-street.csv")
+bristol_11_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-11/2023-11-avon-and-somerset-street.csv")
+bristol_12_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-12/2023-12-avon-and-somerset-street.csv")
+
+#2024 Dataset of Bristol
+bristol_1_2024=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2024_data/2024-01/2024-01-avon-and-somerset-street.csv")
+bristol_2_2024=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2024_data/2024-02/2024-02-avon-and-somerset-street.csv")
+bristol_3_2024=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2024_data/2024-03/2024-03-avon-and-somerset-street.csv")
+bristol_4_2024=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2024_data/2024-04/2024-04-avon-and-somerset-street.csv")
+bristol_5_2024=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2024_data/2024-05/2024-05-avon-and-somerset-street.csv")
+bristol_6_2024=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2024_data/2024-06/2024-06-avon-and-somerset-street.csv")
+
+#-------------------------------------------------------------------------------------------------------------------------------------------
+#Cornwall
+#2022 Dataset of Cornwall
+
+cornwall_1_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-01/2022-01-devon-and-cornwall-street.csv")
+cornwall_2_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-02/2022-02-devon-and-cornwall-street.csv")
+cornwall_3_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-03/2022-03-devon-and-cornwall-street.csv")
+cornwall_4_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-04/2022-04-devon-and-cornwall-street.csv")
+cornwall_5_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-05/2022-05-devon-and-cornwall-street.csv")
+cornwall_6_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-06/2022-06-devon-and-cornwall-street.csv")
+cornwall_7_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-07/2022-07-devon-and-cornwall-street.csv")
+cornwall_8_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-08/2022-08-devon-and-cornwall-street.csv")
+cornwall_9_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-09/2022-09-devon-and-cornwall-street.csv")
+cornwall_10_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-10/2022-10-devon-and-cornwall-street.csv")
+cornwall_11_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-11/2022-11-devon-and-cornwall-street.csv")
+cornwall_12_2022=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2022_data/2022-12/2022-12-devon-and-cornwall-street.csv")
+
+#2023 DataSet of Cornwall
+
+cornwall_1_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-01/2023-01-devon-and-cornwall-street.csv")
+cornwall_2_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-02/2023-02-devon-and-cornwall-street.csv")
+cornwall_3_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-03/2023-03-devon-and-cornwall-street.csv")
+cornwall_4_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-04/2023-04-devon-and-cornwall-street.csv")
+cornwall_5_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-05/2023-05-devon-and-cornwall-street.csv")
+cornwall_6_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-06/2023-06-devon-and-cornwall-street.csv")
+cornwall_7_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-07/2023-07-devon-and-cornwall-street.csv")
+cornwall_8_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-08/2023-08-devon-and-cornwall-street.csv")
+cornwall_9_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-09/2023-09-devon-and-cornwall-street.csv")
+cornwall_10_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-10/2023-10-devon-and-cornwall-street.csv")
+cornwall_11_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-11/2023-11-devon-and-cornwall-street.csv")
+cornwall_12_2023=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2023_data/2023-12/2023-12-devon-and-cornwall-street.csv")
+
+#2024
+cornwall_1_2024=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2024_data/2024-01/2024-01-devon-and-cornwall-street.csv")
+cornwall_2_2024=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2024_data/2024-02/2024-02-devon-and-cornwall-street.csv")
+cornwall_3_2024=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2024_data/2024-03/2024-03-devon-and-cornwall-street.csv")
+cornwall_4_2024=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2024_data/2024-04/2024-04-devon-and-cornwall-street.csv")
+cornwall_5_2024=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2024_data/2024-05/2024-05-devon-and-cornwall-street.csv")
+cornwall_6_2024=read_csv("X:/College projects/4th sem/Data Science/Obtain/Crime Rates/2024_data/2024-06/2024-06-devon-and-cornwall-street.csv")
+
+#c1_24=read_csv("D:/Sem 4/data science/assignment/Obtain/Crime/2024/2024-01/2024-01-devon-and-cornwall-street.csv")
+#c2_24=read_csv("D:/Sem 4/data science/assignment/Obtain/Crime/2024/2024-02/2024-02-devon-and-cornwall-street.csv")
+#c3_24=read_csv("D:/Sem 4/data science/assignment/Obtain/Crime/2024/2024-03/2024-03-devon-and-cornwall-street.csv")
+#c4_24=read_csv("D:/Sem 4/data science/assignment/Obtain/Crime/2024/2024-04/2024-04-devon-and-cornwall-street.csv")
+#c5_24=read_csv("D:/Sem 4/data science/assignment/Obtain/Crime/2024/2024-05/2024-05-devon-and-cornwall-street.csv")
+
+#combinig
+crime_comb= rbind(
+  bristol_1_2022, bristol_2_2022, bristol_3_2022, bristol_4_2022, bristol_5_2022, bristol_6_2022, bristol_7_2022, bristol_8_2022, bristol_9_2022, bristol_10_2022, bristol_11_2022, bristol_12_2022,
+  bristol_1_2023, bristol_2_2023, bristol_3_2023, bristol_4_2023, bristol_5_2023, bristol_6_2023, bristol_7_2023, bristol_8_2023, bristol_9_2023, bristol_10_2023, bristol_11_2023, bristol_12_2023,
+  bristol_1_2024, bristol_2_2024, bristol_3_2024, bristol_4_2024, bristol_5_2024, bristol_6_2024,
+  
+  cornwall_1_2022, cornwall_2_2022, cornwall_3_2022, cornwall_4_2022, cornwall_5_2022, cornwall_6_2022, cornwall_7_2022, cornwall_8_2022, cornwall_9_2022, cornwall_10_2022, cornwall_11_2022, cornwall_12_2022,
+  cornwall_1_2023, cornwall_2_2023, cornwall_3_2023, cornwall_4_2023, cornwall_5_2023, cornwall_6_2023, cornwall_7_2023, cornwall_8_2023, cornwall_9_2023, cornwall_10_2023, cornwall_11_2023, cornwall_12_2023,
+  cornwall_1_2024, cornwall_2_2024, cornwall_3_2024, cornwall_4_2024, cornwall_5_2024, cornwall_6_2024
+)
+
+#changing into tibble
+crime_comb= crime_comb %>% 
+  as_tibble()
+
+post_lsoa = read_csv("X:/College projects/4th sem/Data Science/Obtain/Postcode/Postcode to LSOA.csv")
+population = read_csv("X:/College projects/4th sem/Data Science/Obtain/Population2011_1656567141570.csv")
+#view(post_lsoa)
+
+#selecting only required one
+cleancrime = crime_comb %>%
+  select(Month, `LSOA code`, `Crime type`, `Falls within`)
+cleanlsoa = post_lsoa %>%
+  select(`lsoa11cd`, `lsoa11nm`, `ladnm`, `pcds`)
+
+#changing the columns name
+colnames(cleanlsoa) = c('LSOA code', 'street', 'counties', "postcode")
+colnames(population) = c("postcode", "count")
+
+#filtering the required counties
+pl = cleanlsoa %>%
+  filter(counties %in% c("Bristol, City of", "Cornwall")) %>%
+  mutate(postcode = str_trim((substring(postcode, 1, 6))))
+
+#checking for duplicates
+any(duplicated(cleancrime$`LSOA code`))
+any(duplicated(pl$`LSOA code`))
+
+#removing the duplicates
+cleancrime1=unique(cleancrime, by = "LSOA code")
+pl1=unique(pl, by = "LSOA code")
+
+#cleaning & merging 
+final_cleaned_crime = cleancrime1 %>%
+  left_join(pl1, by = "LSOA code", relationship = "many-to-many") %>%
+  mutate(Year  = str_trim(substring(Month,  1, 4))) %>%
+  mutate(Month  = str_trim(substring(Month,  6, 7))) %>%
+  left_join(population, by = "postcode") %>%
+  filter(!is.na(`Crime type`) & !is.na(`Month`) & !is.na(`Falls within`) & !is.na(`LSOA code`)& !is.na(`street`) & !is.na(`counties`) & !is.na(`count`))
+
+fi_clean_crime = clean_housing_prabin %>%
+  select(Postcode, `Town/City`) %>%
+  mutate(postcode = str_trim(substring(Postcode, 1, 6))) %>%
+  left_join(final_cleaned_crime, by="postcode", relationship = "many-to-many")
+
+#view(fi_clean_crime)
+write.csv(final_cleaned_crime, "X:/College projects/4th sem/Data Science/Data Cleaning/Cleaned/cleanedcrime.csv", row.names = FALSE)
+
+#view(final_cleaned_crime)
+
+#---------------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------
+
+#housing
+col_n=c("ID", "Price", "Year", "Postcode", "Property Type", "Old/New", "Duration", "PAON", "SAON", "Street", "Locality", "Town/City", "District", "County", "PPD Category Type")
+
+h20 = read_csv("X:/College projects/4th sem/Data Science/Obtain/Housing/pp-2020.csv", col_names = col_n)
+h21 = read_csv("X:/College projects/4th sem/Data Science/Obtain/Housing/pp-2021.csv", col_names = col_n)
+h22 = read_csv("X:/College projects/4th sem/Data Science/Obtain/Housing/pp-2022.csv", col_names = col_n)
+h23 = read_csv("X:/College projects/4th sem/Data Science/Obtain/Housing/pp-2023.csv", col_names = col_n)
+
+#merging
+housing = rbind(h20, h21, h22, h23)
+
+#cleaning 
+clean_housing_prabin = housing %>%
+  filter(County %in% c('CORNWALL', 'CITY OF BRISTOL')) %>%
+  select(Price, Year, Postcode, `Town/City`, County) %>%
+  mutate(Year = str_trim(substring(Year, 1, 4))) %>%
+  filter(!is.na(Price) & !is.na(Year) & !is.na(Postcode) & !is.na(`Town/City`) & !is.na(County))
+
+view(clean_housing_prabin)
+
+write_csv(clean_housing_prabin, "X:/College projects/4th sem/Data Science/Data Cleaning/Cleaned/cleanedhousing.csv")
+
+#---------------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------
+
+#broadbandspeed data
+
+broadbandspeed = read_csv("X:/College projects/4th sem/Data Science/Obtain/broadband speed/201809_fixed_pc_r03/201805_fixed_pc_performance_r03.csv")
+
+broad_sel = broadbandspeed %>%
+  select("postcode_space", "Median download speed (Mbit/s)", "Median upload speed (Mbit/s)", "Average upload speed (Mbit/s)", "Average download speed (Mbit/s)", "Maximum upload speed (Mbit/s)", "Maximum download speed (Mbit/s)")%>%
+  filter(!is.na(postcode_space) & !is.na(`Median download speed (Mbit/s)`) & !is.na(`Median upload speed (Mbit/s)`) & !is.na(`Average upload speed (Mbit/s)`) & !is.na(`Average download speed (Mbit/s)`) & !is.na(`Maximum upload speed (Mbit/s)`) & !is.na(`Maximum download speed (Mbit/s)`))
+
+#view(broad_sel)
+broadjoin = broad_sel %>%
+  left_join(clean_housing_prabin %>%
+              select(Postcode, `Town/City`, County),
+            by = c("postcode_space" = "Postcode"))
+
+
+broadbandspeed_clean = broadjoin %>%
+  filter(!is.na(`Town/City`) & !is.na(County))
+
+
+write_csv(broadbandspeed_clean, "X:/College projects/4th sem/Data Science/Data Cleaning/Cleaned/cleanedbroadband_speed.csv")
+
+
+#---------------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------
+
+#School Data
+
+bristol_data21=read_csv("X:/College projects/4th sem/Data Science/Obtain/School/Bristol/2021-2022/801_ks4final.csv")
+bristol_data21=read_csv("X:/College projects/4th sem/Data Science/Obtain/School/Bristol/2022-2023/801_ks4final.csv")
+cornwall_data21=read_csv("X:/College projects/4th sem/Data Science/Obtain/School/Cornwall/2021-2022/908_ks4final.csv")
+cornwall_data22=read_csv("X:/College projects/4th sem/Data Science/Obtain/School/Cornwall/2022-2023/908_ks4final.csv")
+
+bristol_data21 = bristol_data21 %>%
+  select(SCHNAME, PCODE, ATT8SCR, TOWN)%>%
+  mutate(YEAR=2021,  COUNTY="Bristol")
+
+bristol_data22 = bristol_data21 %>%
+  select(SCHNAME, PCODE, ATT8SCR, TOWN)%>%
+  mutate(YEAR=2022, COUNTY="Bristol")
+
+cornwall_data21 = cornwall_data21 %>%
+  select(SCHNAME, PCODE, ATT8SCR, TOWN)%>%
+  mutate(YEAR=2021, COUNTY="Cornwall")
+
+cornwall_data22 = cornwall_data21 %>%
+  select(SCHNAME, PCODE, ATT8SCR, TOWN)%>%
+  mutate(YEAR=2022, COUNTY="Cornwall")
+
+#combinig school data
+combined_bristol = rbind(bristol_data21, bristol_data22)
+combined_cornwall = rbind(cornwall_data21, cornwall_data22)
+
+view(combined_bristol)
+view(combined_cornwall)
+
+cleaned_bristol = combined_bristol %>%
+  filter(!is.na(SCHNAME) & !is.na(PCODE) & !is.na(ATT8SCR)  & !is.na(TOWN)) %>%
+  filter(ATT8SCR != "NE" & ATT8SCR != "SUPP")
+
+cleaned_cornwall = combined_cornwall %>%
+  filter(!is.na(SCHNAME) & !is.na(PCODE) & !is.na(ATT8SCR) & !is.na(TOWN)) %>%
+  filter(ATT8SCR != "NE" & ATT8SCR != "SUPP") %>%
+  distinct()
+
+view(cleaned_cornwall)
+
+both = rbind(cleaned_bristol, cleaned_cornwall)
+view(both)
+dim(both)
+write_csv(both, "X:/College projects/4th sem/Data Science/Data Cleaning/Cleaned/cleanedschool.csv")
